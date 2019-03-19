d7c94e2706f2c10d3369891ab4366b89c0934ab3
AliAbdulle <alidhobey@gmail.com>
2019-03-18 21:51:53 -0500

    Finished work

---
+++ src/scripts/powers.js
@@ -0,0 +1,57 @@
+/*
+Add the correct string as the first argument to addEventListener()
+Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
+Have your developer tools open. When you click the button, the following element...
+*/
+function flightFunction () {
+    const section = document.getElementById("flight")
+    section.classList.remove("disabled")
+    section.classList.add("enabled")
+}
+
+document.getElementById("activate-flight").addEventListener("click", flightFunction)
+/*
+Once you have that working, write two more JavaScript functions, and two more querySelector().addEventListener()
+statements for handling the click event on the other two buttons. Those event handlers should perform the same step of 
+removing disabled, and adding enabled on the corresponding <section> elements
+*/
+
+function mindreadingFunction () {
+    const section = document.getElementById("mindreading")
+    section.classList.remove("disabled")
+    section.classList.add("enabled")
+}
+
+function xrayFunction () {
+    const section = document.getElementById("mindreading")
+    section.classList.remove("disabled")
+    section.classList.add("enabled")
+}
+
+document.querySelector("#activate-mindreading").addEventListener("click", mindreadingFunction)
+document.querySelector("#activate-xray").addEventListener("click", xrayFunction)
+
+/*
+Write two more event handlers for activating and deactivating all powers when the corresponding buttons
+ are clicked. You will need to use the document.querySelectorAll() method for these.
+*/
+
+function activateHandler() {
+    const sections = document.querySelectorAll(".power")
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].classList.remove("disabled")
+        sections[i].classList.add("enabled")
+        
+    }
+}
+function deactivateHandler() {
+    const sections = document.querySelectorAll(".power")
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].classList.remove("enabled")
+        sections[i].classList.add("disabled")
+        
+    }
+}
+
+document.getElementById("activate-all").addEventListener("click", activateHandler)
+document.getElementById("deactivate-all").addEventListener("click", deactivateHandler)
